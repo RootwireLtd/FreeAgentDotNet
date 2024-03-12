9ea0414e8c8dbdd028266478812969bda9e76968
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-03-12 09:30:33 +0000

    Add CategoryService and Tests

---
+++ Rootwire.ApiClients.FreeAgent/src/Rootwire.ApiClients.FreeAgent/Services/Categories/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using RestSharp;
+using Rootwire.ApiClients.FreeAgent.Infrastructure.Public;
+using Rootwire.ApiClients.FreeAgent.Models.Categories;
+
+namespace Rootwire.ApiClients.FreeAgent.Services.Categories;
+
+public class CategoryService
+{
+    private readonly IFreeAgentClient _client;
+    public string BasePath => "/v2/categories";
+
+    public CategoryService(IFreeAgentClient client)
+    {
+        _client = client;
+    }
+    
+    // Create
+    
+    // GetOne
+    
+    // GetMany
+    public async Task<RestResponse<CategoryListResponseModel>> GetAllAsync()
+    {
+        var request = new RestRequest(BasePath, Method.Get);
+
+        var response = await _client.RestClient.ExecuteAsync<CategoryListResponseModel>(request);
+
+        return response;
+    }
+    
+    // Update 
+    
+    // Delete
+
+    
+    
+    
+}
---
+++ Rootwire.ApiClients.FreeAgent/test/Rootwire.ApiClients.FreeAgentTests/Services/Categories/CategoryServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Rootwire.ApiClients.FreeAgent.Services.Categories;
+
+namespace Rootwire.ApiClients.FreeAgentTests.Services.Categories;
+
+public class CategoryServiceTests : BaseFreeAgentTest
+{
+    private const int ContactId = 145970;
+
+    private readonly CategoryService _service;
+
+    public CategoryServiceTests()
+    {
+        _service = new CategoryService(FreeAgentClient);
+    }
+    
+    [Fact]
+    public async Task GetAll()
+    {
+        var response = await _service.GetAllAsync();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(response.Data.GeneralCategories);
+        Assert.NotNull(response.Data.IncomeCategories);
+        Assert.NotNull(response.Data.AdminExpenseCategories);
+        Assert.NotNull(response.Data.CostOfSalesCategories);
+        Assert.NotEqual(0, response.Data.GeneralCategories.Count);
+        Assert.NotEqual(0, response.Data.IncomeCategories.Count);
+        Assert.NotEqual(0, response.Data.AdminExpenseCategories.Count);
+        Assert.NotEqual(0, response.Data.CostOfSalesCategories.Count);
+    }
+}
