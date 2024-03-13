a0b125299f85b9080ee5cbbf4a25fb59e0496deb
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-03-13 08:09:27 +0000

    Add ContactListResponseModel and tests

--- Rootwire.ApiClients.FreeAgent/test/Rootwire.ApiClients.FreeAgentTests/Entities/Contacts/ContactTest.cs
+++ Rootwire.ApiClients.FreeAgent/test/Rootwire.ApiClients.FreeAgentTests/Entities/Contacts/ContactTest.cs
@@ -8,7 +8,7 @@ public class ContactTest : BaseFreeAgentTest
 {
 
     [Fact]
-    public void Deserialize()
+    public void DeserializeOne()
     {
         string json = this.GetFixture("v2/contacts/145970");
 
@@ -20,5 +20,21 @@ public class ContactTest : BaseFreeAgentTest
         Assert.IsType<ContactResponseModel>(contactResponseModel);
         Assert.NotNull(contactResponseModel.Contact.Url);
         Assert.Equal(145970, contactResponseModel.Contact.GetResourceId());
+    }
+
+    [Fact]
+    public void DeserializeList()
+    {
+        string json = this.GetFixture("v2/contacts/");
+
+        JsonElement jsonElement = JsonDocument.Parse(json).RootElement;
+
+        ContactListResponseModel contactListResponseModel =
+            JsonSerializer.Deserialize<ContactListResponseModel>(jsonElement);
+
+        Assert.NotNull(contactListResponseModel);
+        Assert.IsType<ContactListResponseModel>(contactListResponseModel);
+        Assert.Equal(3, contactListResponseModel.Contacts.Count);
+
     }
 }
--- Rootwire.ApiClients.FreeAgent/test/Rootwire.ApiClients.FreeAgentTests/FreeAgentFixtures.cs
+++ Rootwire.ApiClients.FreeAgent/test/Rootwire.ApiClients.FreeAgentTests/FreeAgentFixtures.cs
@@ -32,6 +32,71 @@ public static class FreeAgentFixtures
                    """;
         }
 
+        if (path == "v2/contacts/")
+        {
+            return """
+                   {
+                       "contacts": [
+                           {
+                               "url": "https://api.sandbox.freeagent.com/v2/contacts/151560",
+                               "organisation_name": "Test Org 3",
+                               "active_projects_count": 0,
+                               "created_at": "2024-03-13T07:53:14.000Z",
+                               "updated_at": "2024-03-13T07:53:14.000Z",
+                               "contact_name_on_invoices": true,
+                               "country": "United Kingdom",
+                               "charge_sales_tax": "Auto",
+                               "locale": "en",
+                               "account_balance": "0.0",
+                               "status": "Active",
+                               "uses_contact_invoice_sequence": false,
+                               "emails_invoices_automatically": false,
+                               "emails_payment_reminders": false,
+                               "emails_thank_you_notes": false,
+                               "uses_contact_level_email_settings": false
+                           },
+                           {
+                               "url": "https://api.sandbox.freeagent.com/v2/contacts/145970",
+                               "organisation_name": "TESTORG - KEEP THIS",
+                               "active_projects_count": 0,
+                               "created_at": "2024-03-11T17:40:27.000Z",
+                               "updated_at": "2024-03-11T17:47:55.000Z",
+                               "contact_name_on_invoices": true,
+                               "country": "United Kingdom",
+                               "charge_sales_tax": "Auto",
+                               "locale": "en",
+                               "account_balance": "0.0",
+                               "status": "Active",
+                               "uses_contact_invoice_sequence": false,
+                               "emails_invoices_automatically": false,
+                               "emails_payment_reminders": false,
+                               "emails_thank_you_notes": false,
+                               "uses_contact_level_email_settings": false
+                           },
+                           {
+                               "url": "https://api.sandbox.freeagent.com/v2/contacts/149407",
+                               "organisation_name": "TestOrg 2",
+                               "active_projects_count": 0,
+                               "created_at": "2024-03-12T09:01:05.000Z",
+                               "updated_at": "2024-03-13T07:53:02.000Z",
+                               "email": "test@test.com",
+                               "contact_name_on_invoices": true,
+                               "country": "United Kingdom",
+                               "charge_sales_tax": "Auto",
+                               "locale": "en",
+                               "account_balance": "0.0",
+                               "status": "Active",
+                               "uses_contact_invoice_sequence": false,
+                               "emails_invoices_automatically": false,
+                               "emails_payment_reminders": false,
+                               "emails_thank_you_notes": false,
+                               "uses_contact_level_email_settings": false
+                           }
+                       ]
+                   }
+                   """;
+        }
+
         if (path == "v2/categories")
         {
             return """
