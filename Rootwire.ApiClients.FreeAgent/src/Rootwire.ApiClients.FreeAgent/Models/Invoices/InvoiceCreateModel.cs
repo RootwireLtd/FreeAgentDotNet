2b00a249d992740cc2b5ea7c371ab7ab29bd8b8d
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-03-14 07:46:44 +0000

    Add scaffold for InvoiceService and Request/Response models

---
+++ Rootwire.ApiClients.FreeAgent/src/Rootwire.ApiClients.FreeAgent/Models/Invoices/InvoiceCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using Rootwire.ApiClients.FreeAgent.Services.Invoices;
+
+namespace Rootwire.ApiClients.FreeAgent.Models.Invoices;
+
+public class InvoiceCreateModel
+{
+    [JsonPropertyName("invoice")] public InvoiceCreateOptions Contact { get; set; }
+
+    public InvoiceCreateModel(InvoiceCreateOptions options)
+    {
+        Contact = options;
+    }
+}
---
+++ Rootwire.ApiClients.FreeAgent/src/Rootwire.ApiClients.FreeAgent/Models/Invoices/InvoiceUpdateModel.cs
@@ -0,0 +1,10 @@
+﻿using Rootwire.ApiClients.FreeAgent.Services.Invoices;
+
+namespace Rootwire.ApiClients.FreeAgent.Models.Invoices;
+
+public class InvoiceUpdateModel : InvoiceCreateModel
+{
+    public InvoiceUpdateModel(InvoiceCreateOptions options) : base(options)
+    {
+    }
+}
